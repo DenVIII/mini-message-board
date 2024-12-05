40fd1b367a83e97e3e0b07bab9c0cacee54447e9
Denis <Denis226@gmail.com>
2024-12-05 13:57:53 +0300

    Custom message output templates are configured

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+const PORT = 8081;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Mini Message Board", messages });
+});
+
+app.listen(PORT, () => console.log(`Server is running at: localhost:${PORT}`));
